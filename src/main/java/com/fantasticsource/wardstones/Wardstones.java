4102ac60f002070510b48020e576facffc51b65c
Kyle Koder <silvertallon@gmail.com>
2019-02-22 17:19:04 -0800

    Initial commit

---
+++ src/main/java/com/fantasticsource/wardstones/Wardstones.java
@@ -0,0 +1,11 @@
+package com.fantasticsource.wardstones;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Wardstones.MODID, name = Wardstones.NAME, version = Wardstones.VERSION)
+public class Wardstones
+{
+    public static final String MODID = "wardstones";
+    public static final String NAME = "Wardstones";
+    public static final String VERSION = "1.12.2.000";
+}
