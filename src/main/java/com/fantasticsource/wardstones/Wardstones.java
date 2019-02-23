1c976d832264237ae97b64dd019ca8091e97b0f5
Kyle Koder <silvertallon@gmail.com>
2019-02-22 19:21:50 -0800

    Added newest fantasticlib Added basics for storing/retreiving data from a file in the "data" folder of the current server world (not dimension world)

--- src/main/java/com/fantasticsource/wardstones/Wardstones.java
+++ src/main/java/com/fantasticsource/wardstones/Wardstones.java
@@ -1,11 +1,47 @@
 package com.fantasticsource.wardstones;
 
+import com.fantasticsource.mctools.MCTools;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = Wardstones.MODID, name = Wardstones.NAME, version = Wardstones.VERSION)
+import java.io.File;
+
+@Mod(modid = Wardstones.MODID, name = Wardstones.NAME, version = Wardstones.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.001,)")
 public class Wardstones
 {
     public static final String MODID = "wardstones";
     public static final String NAME = "Wardstones";
     public static final String VERSION = "1.12.2.000";
+
+    public static final String FILENAME = "wardstones.dat";
+    public static File file;
+
+    public Wardstones()
+    {
+//        MinecraftForge.EVENT_BUS.register(Wardstones.class);
+    }
+
+    @Mod.EventHandler
+    public static void onServerStart(FMLServerStartingEvent event)
+    {
+        file = MCTools.getDataFile(event.getServer(), FILENAME);
+        if (file.exists()) load();
+        else
+        {
+            generate();
+            save();
+        }
+    }
+
+    public static void generate()
+    {
+    }
+
+    public static void save()
+    {
+    }
+
+    public static void load()
+    {
+    }
 }
